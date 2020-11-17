4a4d8e5192b63b29a0a9debb187e0103b4945abe
Tetiana Kobrin <t.kobrin10gmai.com>
2020-11-17 18:36:07 +0200

    .

---
+++ #1_1.js
@@ -0,0 +1,19 @@
+document.write(`**************`); 
+
+let r = 10;
+
+let s = Math.PI * r * r;
+
+let h = 15;
+
+document.write(`Обьем цилиндра с площадью основы ${s}, радиусом ${r} и высотой ${h} равен:`);
+
+document.write(`--------------------`);
+
+let v = s * h;
+
+document.write(`${v} = результат.`);
+
+document.write(`--------------------`);
+
+document.write(`end.`);
